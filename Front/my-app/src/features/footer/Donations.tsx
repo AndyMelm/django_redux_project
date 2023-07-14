52fbac9f59acf303c52208ee6a1fdae54f1fb60c
AndyMelm <recast321@gmail.com>
2023-07-14 18:35:31 +0300

    donations with 5$

--- Front/my-app/src/features/footer/Donations.tsx
+++ Front/my-app/src/features/footer/Donations.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
 
-
 const Donations = () => {
-  
+  const createOrder = (data: any, actions: any) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "5.00", // Replace with your desired default donation amount
+          },
+        },
+      ],
+    });
+  };
+
+  const handleApprove = (data: any, actions: any) => {
+    return actions.order.capture().then(() => {
+      alert('Thank you for your donation! Your payment was successful.');
+    });
+  };
+
   return (
-    <div>
-            Donations
-      <PayPalScriptProvider options={{"clientId": "AQ9fxDAyVLo657VegI0KXi8AiX8ru71b_jgxWIe0QYdSGB5xAKOrYAY7ihKBteS3BamzYQCPkMfrHVYl"}}>
-        <PayPalButtons></PayPalButtons>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '50vh', flexDirection: 'column' }}>
+      <h2>Donations</h2>
+      <p>We are constantly upgrading and learning to build new features.<br />
+        You can donate $5 to support the team!<br />
+        We appreciate your donations!</p>
+      <PayPalScriptProvider options={{
+        clientId: "AQ9fxDAyVLo657VegI0KXi8AiX8ru71b_jgxWIe0QYdSGB5xAKOrYAY7ihKBteS3BamzYQCPkMfrHVYl",
+        disableFunding: 'card', // Disable credit card option
+      }}>
+        <PayPalButtons createOrder={createOrder} onApprove={handleApprove} />
       </PayPalScriptProvider>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Donations
+export default Donations;
--- Front/my-app/src/features/footer/Tutorial.tsx
+++ Front/my-app/src/features/footer/Tutorial.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Tutorial = () => {
+  const iconStyle = {
+    fontSize: '30px',
+    color: 'red',
+  };
+
   return (
     <div>
       <h2>Tutorial</h2>
       <p>
-        Welcome to the tutorial section! We have prepared a detailed video tutorial to help you get started with using our app. Follow along with the instructions in the video to make the most out of the features and functionalities.
+        Welcome to the tutorial section! We have prepared a detailed video tutorial to help you get started with using our app. <br /><br />
+        Follow along with the instructions in the video to make the most out of the features and functionalities.
       </p>
       <p>
-        Watch the tutorial video on YouTube: 
+        Watch the tutorial video on YouTube: &nbsp; 
         <a href="https://www.youtube.com/watch?v=your-video-id" target="_blank" rel="noopener noreferrer">
-          [Your Tutorial Video Title]
+          <FontAwesomeIcon icon={faYoutube} style={iconStyle}  />
         </a>
-      </p>
-      <p>
-        If you have any additional questions or need further assistance, feel free to reach out to our support team. We're here to help you succeed in using our app!
       </p>
     </div>
   );
